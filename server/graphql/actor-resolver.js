29a2ac42f676d93c378bf3fb4441dff30eeaeb17
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-09-27 16:44:01 +1000

    Added try catches to resolver

--- server/graphql/actor-resolver.js
+++ server/graphql/actor-resolver.js
@@ -3,20 +3,55 @@ const { Actor, validate } = require("../models/actor");
 const actorResolver = {
   Query: {
     findActorById: async (root, arguments) => {
-      const actor = await Actor.findById(arguments.id);
-      if (!actor) {
-        console.error("Error: No Actor has been found.");
+      try {
+        const actor = await Actor.findById(arguments.id);
+        if (!actor) {
+          console.error("Error: No Actor has been found.");
+        }
+        return actor;
+      } catch (err) {
+        console.error(err);
       }
-      return actor;
     },
     findAllActor: async (root, arguments) => {
-      const actors = await Actor.find({});
-      if(!actors) {
-        console.error("Error: No Actors could be found.")
+      try {
+        const actors = await Actor.find({});
+        if (!actors) {
+          console.error("Error: No Actors could be found.");
+        }
+        return actors;
+      } catch (err) {
+        console.error(err);
       }
-      return actors;
     },
-    searchActorByFirstName: async(root, arguments) => {}
+    searchActorByFirstName: async (root, { firstName }) => {
+      try {
+        const firstNameRegExp = `.*${firstName}.*`;
+        const regExp = new RegExp(firstNameRegExp, "g");
+        const actor = await Actor.find({ firstName: regExp });
+        if (actor.length === 0 || actor.length <= 0) {
+          console.log("Error: No Actor with at first name could be found.");
+        } else {
+          return actor;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    searchActorByLastName: async (root, { lastName }) => {
+      try {
+        const lastNameRegExp = `.*${lastName}.*`;
+        const regExp = new RegExp(lastNameRegExp, "g");
+        const actor = await Actor.find({lastName: regExp});
+        if (actor.length === 0 || actor.length <= 0) {
+          console.log("Error: No Actor with at first name could be found.");
+        } else {
+          return actor;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    },
   },
   Mutation: {
     addActor: async (root, arguments) => {
