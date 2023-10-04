2a997bd524ffebfe0e8cadcf1e8ecd2440c11c45
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-10-04 11:58:19 +1100

    added manditory packages

--- client/review-it-client/src/App.jsx
+++ client/review-it-client/src/App.jsx
@@ -1,12 +1,57 @@
 // REACT ROUTER DOM
-import { Routes, Route } from "react-router-dom";
-
+import { Routes, Route, Navigate } from "react-router-dom";
+// REACT HOOKS
+import { useState, useEffect } from "react";
+// APOLLO SERVER
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+// LAYOUT
+import Layout from "./components/layout/Layout";
 // PAGES
 import HomePage from "./pages/HomePage";
 
-// LAYOUT
-import Layout from "./components/layout/Layout";
+const client = new ApolloClient({
+  uri: "http://localhost:5000",
+  cache: new InMemoryCache(),
+});
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  const handleLogin = (user) => {
+    setUser(user);
+    saveTokenSession(user);
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+    sessionStorage.removeItem("user");
+    client.clearStore();
+  };
+
+  function saveTokenSession(user) {
+    sessionStorage.setItem("user", JSON.stringify(user));
+  }
+
+  const getUserFromSessionStorage = () => {
+    try {
+      const string = sessionStorage.getItem("user");
+      const user = JSON.parse(string);
+      return user;
+    } catch (err) {
+      console.err(err);
+      sessionStorage.setItem("User", "");
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const user = getUserFromSessionStorage();
+    if (user) {
+      setUser(user);
+    }
+  }, []);
+
+  // PROTECTED ROUTES FUNCTION WHEN 
   return (
     <Routes>
       <Route>
--- server/models/tvshow.js
+++ server/models/tvshow.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Joi = require("joi");
+
+function validateShow(show) {
+  const schema = Joi.object({
+    title: Joi.string().min(1).max(100).required(),
+  });
+}
 
 const tvShowSchema = new Schema({
-  name: { type: String },
+  title: { type: String, minLength: 1, maxLength: 100, required: true },
   description: {
     type: String,
   },
