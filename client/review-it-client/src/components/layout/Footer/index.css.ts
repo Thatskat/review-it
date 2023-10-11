bd3e5db4c96a7a35c5756d91785fe36593d54906
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-10-12 10:16:49 +1100

    put all links into a list to stop that weird thing

--- client/review-it-client/src/components/layout/Footer/index.css.ts
+++ client/review-it-client/src/components/layout/Footer/index.css.ts
@@ -2,24 +2,27 @@ import { style, globalStyle } from "@vanilla-extract/css";
 import { variables } from "../../../styles/themes.css";
 
 export const footer = style({
-  borderTop: `${variables.colors.secondary} solid 1px`,
   background: variables.colors.primary,
   padding: "2rem 4rem",
   color: variables.colors.secondary,
   display: "grid",
   gridTemplateColumns: "repeat(4, 1fr)",
-  textAlign: "end"
+  textAlign: "end",
 });
 
 globalStyle(`${footer} h4`, {
   color: variables.colors.secondary,
   fontFamily: variables.fonts.brand,
-  textTransform: "lowercase"
+  textTransform: "lowercase",
 });
 globalStyle(`${footer} a`, {
-   color: variables.colors.brand,
-   textTransform: "lowercase",
-   display: "block",
-   fontSize: "0.8em",
-   padding: "0.2rem 0"
-  });
+  color: variables.colors.brand,
+  textTransform: "lowercase",
+  display: "block",
+  fontSize: "0.8em",
+  padding: "0.2rem 0",
+});
+
+globalStyle(`${footer} a:hover`, {
+  textDecoration: "underline",
+});
--- client/review-it-client/src/components/layout/Footer/index.jsx
+++ client/review-it-client/src/components/layout/Footer/index.jsx
@@ -2,29 +2,69 @@ import * as styles from "./index.css";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <footer className={styles.footer}>
-    <div></div>
-    <div>
-      <h4>Find out more</h4>
-      <Link to="/about">About Us</Link>
-      <Link to="/contact">Contact Us</Link>
-      <Link to="/privacy-policy">Privacy Policy</Link>
-      <Link to="/terms-conditions">Terms & Conditions</Link>
-    </div>
-    <div>
-      <h4>Site links</h4>
-      <Link to="/">Home</Link>
-      <Link to="/shows">Contact Us</Link>
-      <Link to="/privacy-policy">Privacy Policy</Link>
-      <Link to="/terms-conditions">Terms & Conditions</Link>
-    </div>
-    <div>
-      <h4>Follow Us</h4>
-      <a href="#" target="_blank" rel="noreferrer">Instagram</a>
-      <a href="#" target="_blank" rel="noreferrer">Facebook</a>
-      <a href="#" target="_blank" rel="noreferrer">X</a>
-    </div>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <div></div>
+      <div>
+        <h4>Find out more</h4>
+        <ul>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/privacy-policy">Privacy Policy</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/terms-conditions">Terms & Conditions</Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h4>Site links</h4>
+        <ul>
+          <li>
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/shows">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/privacy-policy">Privacy Policy</Link>
+          </li>
+          <Link to="/terms-conditions">Terms & Conditions</Link>
+          <li></li>
+        </ul>
+      </div>
+      <div>
+        <h4>Follow Us</h4>
+        <ul>
+          <li>
+            {" "}
+            <a href="#" target="_blank" rel="noreferrer">
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" rel="noreferrer">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href="#" target="_blank" rel="noreferrer">
+              X
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
