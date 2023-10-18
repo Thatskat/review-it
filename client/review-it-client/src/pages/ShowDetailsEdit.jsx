6756403fc8c0d52d601a764cf997ab61d9cfc240
Thatskat <109633947+Thatskat@users.noreply.github.com>
2023-10-18 14:54:28 +1100

    navigation for none non-admin user

--- client/review-it-client/src/pages/ShowDetailsEdit.jsx
+++ client/review-it-client/src/pages/ShowDetailsEdit.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Helmet } from "react-helmet";
+import { useNavigate } from "react-router-dom";
 
-const ShowDetailsEdit = () => {
-  return (
-    <div>ShowDetailsEdit</div>
-  )
-}
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_TV_SHOW } from "../graphql/queries";
+import { EDIT_TV_SHOW } from "../graphql/mutations";
 
-export default ShowDetailsEdit
+const ShowDetailsEdit = ({ user }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (user?.isAdmin !== true) {
+      navigate(`/profile/${user?._id}`);
+    }
+  }, []);
+  return <div>ShowDetailsEdit</div>;
+};
+
+export default ShowDetailsEdit;
